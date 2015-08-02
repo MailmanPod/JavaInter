a5a58a5500ab4230b90b94a46a48bc3f070d8f60
Razorback <brueradamian@gmail.com>
2015-08-02 20:30:50 -0300

    Change Thread Execution 2

--- src/main/java/ser/jint/wizardmodels/CreateOrder.java
+++ src/main/java/ser/jint/wizardmodels/CreateOrder.java
@@ -15,16 +15,17 @@ import ser.jint.bo.Order;
  * Created by Razor15 on 29/07/2015.
  */
 public class CreateOrder {
-
+	
 	private static final int	FORM_WIDTH	= 800;
-    private static final int FORM_HEIGHT = 650;
-
-    public CreateOrder(){
-
-    }
-    public void main(){
-        Runnable r = new Runnable() {
-            public void run() {
+	private static final int	FORM_HEIGHT	= 650;
+	
+	public CreateOrder() {
+	
+	}
+	
+	public void main() {
+		Runnable r = new Runnable() {
+			public void run() {
 				
 				try {
 					UIManager.setLookAndFeel(
@@ -35,24 +36,25 @@ public class CreateOrder {
 					e.printStackTrace();
 				}
 				
-                WizardOrder wo = new WizardOrder();
-                Wizard wizard = wo.createWizard();
-
+				WizardOrder wo = new WizardOrder();
+				Wizard wizard = wo.createWizard();
+				
 				Dimension tamPantalla = Toolkit.getDefaultToolkit()
 						.getScreenSize();
 						
-				Rectangle r = new Rectangle((tamPantalla.width - FORM_WIDTH) / 2,
-						(tamPantalla.height - FORM_HEIGHT) / 2, FORM_WIDTH, FORM_HEIGHT);
+				Rectangle r = new Rectangle(
+						(tamPantalla.width - FORM_WIDTH) / 2,
+						(tamPantalla.height - FORM_HEIGHT) / 2, FORM_WIDTH,
+						FORM_HEIGHT);
 						
-                Map prop = new MergeMap("propiedades");
-                prop.put("newOrder", new Order());
-
-                WizardDisplayer.showWizard(wizard, r, null, prop);
-            }
-        };
-		/*
-		 * Thread th = new Thread(r); th.start();
-		 */
-		EventQueue.invokeLater(r);
-    }
+				Map prop = new MergeMap("propiedades");
+				prop.put("newOrder", new Order());
+				
+				WizardDisplayer.showWizard(wizard, r, null, prop);
+			}
+		};
+		Thread th = new Thread(r);
+		th.start();
+		/* EventQueue.invokeLater(r); */
+	}
 }
